436fd96e4029bddf59680d8f5bc3e349fda8ea50
Ajay MT <ajay.tatachar@gmail.com>
2015-05-11 17:11:24 +0530

    connect to servers without having to bind

--- ports.py
+++ ports.py
@@ -9,23 +9,33 @@ class Ports(nodenet.Node):
         super(Ports, self).__init__(loop)
 
         self.services = {}
-        self.on('register', self.on_register)
+        self.ports = []
+        self.on('register', self._on_register)
+        self.on('bound', self._on_bound)
 
-    def on_register(self, who, service):
+    def _on_register(self, who, service):
         peers = [p for p in self.peers if not who == p]
         self.emit('register', service, to=peers)
 
-    def register(self, name, **kwargs):
-        if not self.sockname[0]:
-            return None
+    def _on_bound(self, who):
+        host, port = who
+        self.ports.append(port)
 
-        ports = []
-        for s in self.services.values():
-            ports += [p['port'] for p in s]
+    def _bind(self):
+        port = randrange(10000, 65535)
+        while port in self.ports:
+            port = randrange(10000, 65535)
+
+        self.bind('127.0.0.1', port)
+        self.emit('bound')
+
+    def register(self, name, **kwargs):
+        if self.sockname == (None, None):
+            return None
 
         rng = kwargs.get('range')
         port = kwargs.get('port')
-        while port in ports or port is None:
+        while port in self.ports or port is None:
             port = randrange(*(rng or (10000, 65535)))
 
         service = {'port': port, 'id': str(uuid4())}
@@ -34,6 +44,13 @@ class Ports(nodenet.Node):
             self.services[name] = []
 
         self.services[name].append(service)
+        self.ports.append(port)
         self.emit('register', service)
 
         return port
+
+    def connect(self, *who):
+        if self.sockname == (None, None):
+            self._bind()
+
+        super(Ports, self).connect(*who)
