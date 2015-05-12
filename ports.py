bcd1cbfa064c51096912364f67936130a92669ff
Ajay MT <ajay.tatachar@gmail.com>
2015-05-12 12:40:46 +0530

    fix bugs, make the name a property of a service

--- ports.py
+++ ports.py
@@ -17,24 +17,23 @@ class Ports(nodenet.Node):
         self.on('close', self._on_close)
 
     def _on_register(self, who, service):
-        for name, services in self.services.items():
-            for s in services:
-                if s == service:
-                    s['_peer'] = who
+        if service['name'] not in self.services:
+            self.services[service['name']] = []
 
-        service['_peer'] = self.sockname
+        self.services[service['name']].append(service)
+
+        s = {k: v for k, v in service.items()}
+        s['_peer'] = self.sockname
 
         peers = [p for p in self.peers if not who == p]
-        self.emit('register', service, to=peers)
+        self.emit('register', s, to=peers)
 
     def _on_bound(self, who):
         host, port = who
         self.ports.append(port)
 
     def _on_unregister(self, who, service):
-        for name, services in self.services.items():
-            if service in services:
-                services.remove(service)
+        self.services[service['name']].remove(service)
 
     def _on_close(self, *args):
         def lsum(l): return l[0] + lsum(l[1:]) if l else []
@@ -62,9 +61,10 @@ class Ports(nodenet.Node):
         while port in self.ports or port is None:
             port = randrange(*(rng or (10000, 65535)))
 
-        service = {'port': port, 'id': str(uuid4()), '_peer': self.sockname}
+        service = {'port': port, 'id': str(uuid4()), '_peer': self.sockname,
+                   'name': name}
 
-        if not self.services.get(name):
+        if name not in self.services:
             self.services[name] = []
 
         self.services[name].append(service)
